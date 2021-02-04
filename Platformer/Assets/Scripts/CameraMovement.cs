fbab32fefa8261fd66593167b6bd5df56f36c64f
supermancantswim <oramos1@ivc.edu>
2021-02-04 12:00:05 -0800

    Updated Camera
    Camera has now been updated to follow player vertically. Limits have been set to match level.

---
+++ Platformer/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform target;
+    [SerializeField] private float smoothing;
+
+    [SerializeField] private Vector2 maxPosition;
+    [SerializeField] private Vector2 minPosition;
+
+    private void Start()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if(transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x,
+                                                 target.position.y,
+                                                 transform.position.z);
+            targetPosition.x = Mathf.Clamp(target.position.x,
+                                           minPosition.x, 
+                                           maxPosition.x);
+            targetPosition.y = Mathf.Clamp(target.position.y,
+                                          minPosition.y,
+                                          maxPosition.y);
+            
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+
+        }
+    }
+}
