de5b16d54acb61dcd6742b070edbb8da43af5b6c
Mihir Thanekar <mihirthanekar@gmail.com>
2021-02-10 23:41:04 -0800

    breaking platforms added

---
+++ Platformer/Assets/BreakingPlatform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakingPlatform : MonoBehaviour {
+	private bool isDebugging = false;
+	private float destroyPlatformInterval = 3.0f;	// number of seconds to wait for platform destruction
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			if(isDebugging) print("Player interaction with platform.");
+			Invoke("destroyPlatform", 1.0f);
+		}	
+	}
+
+	private void destroyPlatform() {
+		Destroy(this.gameObject);
+	}
+}
+ 
