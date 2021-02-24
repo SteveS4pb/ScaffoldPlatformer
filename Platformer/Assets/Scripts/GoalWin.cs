5ba04499f3e92f75a6e9dff7378cac227c7e5aea
supermancantswim <oramos1@ivc.edu>
2021-02-24 14:14:47 -0800

    Adds a win condition
    Added goal for the character to reach and win.
    Note: Must press spacebar while character is on goal to win.

---
+++ Platformer/Assets/Scripts/GoalWin.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalWin : MonoBehaviour {
+
+    public bool playerInRange;
+
+	// Use this for initialization
+	void Start () {
+        playerInRange = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space) && playerInRange)
+        {
+            Debug.Log("YOU WIN TEST");
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+
+    }
+}
